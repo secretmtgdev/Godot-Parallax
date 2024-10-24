e902d0e3f09f9d0b52e627521fe4cc47f2d123a3
secretmtgdev <secretmtgdev@gmail.com>
2024-10-23 18:25:43 -0700

    Added a basic parallax project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Description
+This is a repo for testing purposes.
+
+## Project sample videos
+- [City Parallax Test](https://www.youtube.com/watch?v=mNK6a6W4eEo)
+
+## Itch.io demo links
---
+++ parallaxtrain/singletons/GameManager.gd
@@ -0,0 +1,8 @@
+extends Node
+
+const BG_IMG_HEIGHT: float = 324.0
+const BG_IMG_WIDTH: float = 576.0
+const SCROLL_SPEED: float = 100.0
+
+const VIEWPORT_HEIGHT: float = 648
+const VIEWPORT_WIDTH: float = 1152
